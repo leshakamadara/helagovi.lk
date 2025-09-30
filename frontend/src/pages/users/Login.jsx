d4e6e4eb2b7a503b66f162b302db02513b5e6695
leshakamadara <leshakamadara@gmail.com>
2025-10-01 02:40:24 +0530

    Update login page banner design

--- frontend/src/pages/users/Login.jsx
+++ frontend/src/pages/users/Login.jsx
@@ -22,10 +22,27 @@ export default function LoginPage() {
       </div>
       <div className="bg-muted relative hidden lg:block">
         <img
-          src="https://res.cloudinary.com/dckoipgrs/image/upload/v1759260444/banner_gypb09.png"
+          src="https://res.cloudinary.com/dckoipgrs/image/upload/v1759263222/banner_gypb09.png"
           alt="Helagovi.lk Banner"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
+        <div className="absolute inset-0 flex flex-col items-center justify-center gap-8 md:gap-12">
+          <div 
+            className="font-black tracking-[-4%] text-transparent bg-clip-text text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl text-center pb-2"
+            style={{
+              background: 'radial-gradient(circle, #34E89E 0%, #45A21A 50%, #005B26 100%)',
+              WebkitBackgroundClip: 'text',
+              WebkitTextFillColor: 'transparent'
+            }}
+          >
+            Ceylon's Farming Network
+          </div>
+          <img
+            src="https://res.cloudinary.com/dckoipgrs/image/upload/v1759143086/Logo_uf3yae.png"
+            alt="Helagovi.lk Logo"
+            className="w-56 h-56 md:w-72 md:h-72 lg:w-96 lg:h-96 object-contain"
+          />
+        </div>
       </div>
     </div>
   )
